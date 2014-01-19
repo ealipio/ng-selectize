8fe3c75531564c5c0fbef3cb1f761f041b5f880f
Mario Bartlack <github@preventdefault.de>
2014-01-19 21:26:23 +0100

    demo page

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,22 @@
-console.log('This would be the main JS file.');
+angular.module('ngSelectizeDemo', ['ngSelectize'])
+
+.controller('DemoCtrl', function($scope) {
+
+    function getRandomInt(min, max) {
+      return Math.floor(Math.random() * (max - min + 1) + min);
+    }
+
+    $scope.selectedOption = undefined;
+
+    $scope.options = [
+      {label: 'Option 1', value: 1},
+      {label: 'Option 2', value: 2},
+      {label: 'Option 3', value: 3},
+      {label: 'Option 4', value: 4},
+      {label: 'Option 5', value: 5}
+    ];
+
+    $scope.selectRandomOption = function() {
+      $scope.selectedOption = $scope.options[getRandomInt(0, $scope.options.length - 1)];
+    };
+});
